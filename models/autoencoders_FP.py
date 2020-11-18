002a58b8351e2f3ef3863e11161e79990063e02c
bugbug-lou <58448037+bugbug-lou@users.noreply.github.com>
2020-11-18 08:48:44 +0800

    Update autoencoders_FP.py

--- models/autoencoders_FP.py
+++ models/autoencoders_FP.py
@@ -1,9 +1,10 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import math
 import numpy as np
-from tools import get_all_axis
+
 
 
 def get_fc(x, y, k):
@@ -36,13 +37,17 @@ class Net(nn.Module):
 
 
 class AE(object):
-    def __init__(self, input_dim, hidden_dim, compress_dim, k, thres):
+    def __init__(self, dataset, input_dim, hidden_dim, compress_dim,thres):
         self.compress_net = Net(input_dim, hidden_dim, compress_dim)
         self.retrieve_net = Net(compress_dim, hidden_dim, input_dim)
         self.optimiser = optim.Adam(list(self.compress_net.parameters()) + list(self.retrieve_net.parameters()))
         self.criterion = nn.MSELoss()
-        self.k = k # specifying the test function, torch.tensor
+        #self.k = k # specifying the test function, torch.tensor
         self.thres = thres # specifying the fourier coefficients that are incorporated in the loss
+        self.axes = get_all_axis(compress_dim, thres)
+        #self.testfunc = torch.sin(torch.matmul(torch.FloatTensor(dataset), k))
+        self.data = torch.FloatTensor(dataset)
+        
 
     # Use this method to compress a dataset.
     def compress(self, data):
@@ -50,41 +55,41 @@ class AE(object):
         return self.compress_net(data)
 
     def retrieve(self, data):
-        data = torch.FloatTensor(data)
         return nn.Sigmoid()(self.retrieve_net(data))
 
     # Dataset is a collection of data.
-    def train(self, dataset, iter):
+    def train(self, iter, k):
+        self.k = k # specifying the test function, torch.tensor
+        self.testfunc = torch.sin(torch.matmul(torch.FloatTensor(self.data), k))
+        k = int(iter/10)
         for i in range(iter):
-            loss = self.loss(dataset)
+            if i%k == 0:
+                print('i completed')
+            loss = self.loss()
             self.optimiser.zero_grad()
             loss.backward()
             self.optimiser.step()
 
     # Compute loss
-    def loss(self, dataset):
-        dataset = torch.FloatTensor(dataset)
-        compressed = self.compress(dataset)
-        FP_loss = self.FP(dataset, compressed, self.k, self.thres)
+    def loss(self):
+        compressed = self.compress(self.data)
+        FP_loss = self.FP(compressed)
         retrieved = self.retrieve(compressed)
-        loss = self.criterion(dataset, retrieved)
+        loss = self.criterion(self.data, retrieved)
         loss = loss + FP_loss
         return loss
 
-    def FP(self, dataset, compressed, k, thres):
+    def FP(self,compressed):
         ## thres: the axis entry maximum value
         ## k: specifying the function
         ## get_all: function to be completed
-        testfunc = torch.sin(torch.matmul(dataset, k))
-        dim = compressed.shape[1]
-        axes = get_all_axis(dim, thres)
         FP_loss = torch.tensor([0.0])
-        for arr in axes:
+        for arr in self.axes:
             arr = torch.FloatTensor(arr)
-            coeff = get_fc(compressed, testfunc, arr)
+            coeff = get_fc(compressed, self.testfunc, arr)
             coeff = coeff * torch.sum(torch.square(arr))
             FP_loss = FP_loss + coeff
-        return FP_loss
+        return torch.sqrt(FP_loss)
 
     def MSE(self, dataset):
         with torch.no_grad():
@@ -94,5 +99,6 @@ class AE(object):
             retrieved = self.retrieve(compressed)
             loss = self.criterion(dataset, retrieved)
         return loss
+
 
 
